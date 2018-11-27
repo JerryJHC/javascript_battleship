61856ef094f1f8472a8667c6831f49d2e29313de
Jerry Henríquez <jerryhenriquez@gmail.com>
2018-11-27 18:21:43 +0100

    agregadas funciones para atacar y posicionar barcos y modificado para que los tableros tengan un tamaño fijo y unicamente se le pase el id de la tabla de html donde se dibujara

--- JS/app_o.js
+++ JS/app_o.js
@@ -15,11 +15,13 @@ class player{
 }
 
 class tablero{
-    constructor( rows , columns ){
-        if( rows > 0 && columns > 0 )
-            this.createTable(rows,columns);
+    //Para crear el tablero se le pasa el id de la tabla donde dibujar en html
+    constructor( tableID ){
+        this.tableID = tableID;
+        this.createTable( 5 , 5 );
     }
 
+    //funcion que crea un array que represente el tablero
     createTable(rows,columns){
         this.table = new Array(rows);
         for( let i = 0 ; i < rows ; i++ ){
@@ -29,10 +31,40 @@ class tablero{
         }
     }
 
+    //funcion para validar si el tablero se puede utilizar
     valid(){
-        return ( this.table !== undefined ) ? true : false;
+        return ( this.table !== undefined && tableID != '' ) ? true : false;
     }
 
+    //Funcion para atacar una posicion del tablero
+    attack(x,y){
+        if( x < j1.length )
+            if( y < j1[x].length ){
+                if( j1[x][y] == 'B' ){
+                    j1[x][y] = 'X';
+                    return 'X';
+                }
+            }
+        return 'O';
+    }
+
+    //funcion para agregar barcos en el tablero
+    addShip(x,y,length,direction){
+        //determina las posiciones finales del barco
+        finalX = x;
+        finalY = y;
+        if( direction == 'v' )
+            finalX += length -1;
+        else
+            finalY += length -1;
+        
+        if( finalX < j1.length )
+            if( finalY < j1[x].length ){
+                for( let i = x ; i <= finalX ; i++ )
+                    for( let j = y ; j <= finalY ; j++ )
+                        j1[i][j] = 'B';
+            }
+    }
 }
 
 //Exportando modulos para test
