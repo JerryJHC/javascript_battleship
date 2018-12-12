32990dd0acb88acdd1aece42490a40e8960b7253
Jerry Henríquez <jerryhenriquez@gmail.com>
2018-12-12 21:19:20 +0100

    corregidos eventos sobre la tabla desde el objeto tablero

--- JS/tablero.js
+++ JS/tablero.js
@@ -38,10 +38,10 @@ class tablero{
 
     //Funcion para atacar una posicion del tablero
     attack(x,y){
-        if( x < j1.length )
-            if( y < j1[x].length ){
-                if( j1[x][y] == 'B' ){
-                    j1[x][y] = 'X';
+        if( x < this.table.length )
+            if( y < this.table[x].length ){
+                if( this.table[x][y] == 'B' ){
+                    this.table[x][y] = 'X';
                     return 'X';
                 }
             }
@@ -58,13 +58,48 @@ class tablero{
         else
             finalY += length -1;
         
-        if( finalX < j1.length )
-            if( finalY < j1[x].length ){
+        if( finalX < this.table.length )
+            if( finalY < this.table[x].length ){
                 for( let i = x ; i <= finalX ; i++ )
                     for( let j = y ; j <= finalY ; j++ )
-                        j1[i][j] = 'B';
+                        this.table[i][j] = 'B';
             }
     }
+
+    drawGame() {
+        var tablero = document.getElementById(this.tableID);
+    
+        while (tablero.hasChildNodes()) tablero.removeChild(tablero.firstChild);
+      
+        for (let i = -1; i < this.table.length ; i++) {
+            var fila = document.createElement("tr");
+            for (let j = -1; j < this.table[0].length; j++) {
+                var col = document.createElement("td");
+                fila.appendChild(col);
+                if (i === -1 && j === -1) {
+                } else if (i === -1 && j !== -1) {
+                    col.textContent = String.fromCharCode(65 + j);
+                } else if (j === -1 && i !== -1) {
+                    col.textContent = i;
+                } else {
+                    col.row = i;
+                    col.column = j;
+                    col.textContent = "" + i + ',' + j ;
+                    col.game = this;
+                    col.addEventListener("click", this.handlerCell );
+                }
+            }
+        
+            tablero.appendChild(fila);
+        }
+    }
+
+    handlerCell(e) {
+        let v = e.srcElement;
+        console.log('pulsado :  ' + v.row + ':' + v.column);
+        v.textContent = v.game.attack(v.row ,v.column);
+        v.removeEventListener('click', v.game.handlerCell);
+    };
 }
 
 //Exportando modulos para test
